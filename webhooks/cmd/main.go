3487ab6f09e0cd48465d37f7871d307dc09df4f4
Joshua Pagan <56805199+paganjoshua@users.noreply.github.com>
2021-09-08 16:31:26 -0400

    Created Registry Playbook (#9)
    - created registry playbook to handle local
      registry updates on devl push webhook

--- webhooks/cmd/main.go
+++ webhooks/cmd/main.go
@@ -8,11 +8,12 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/main", mainHandler)
+	http.HandleFunc("/devl", devlHandler)
 	log.Fatal(http.ListenAndServe(":8008", nil))
 }
 
-func homeHandler(w http.ResponseWriter, r *http.Request) {
+func mainHandler(w http.ResponseWriter, r *http.Request) {
 	var ansi ansible.Runner
 	params := ansible.Params{
 		User:	"jynx",
@@ -20,6 +21,22 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 		Cmd:	ansible.Cmd{
 			AnsibleCommand: "playbook",
 			Args: []string{	"-u", "jynx", "--tags", "deploy", "webserver.yml" },
+		},
+	}
+
+	ansi = ansible.New(params)
+	ansi.Run()
+	w.WriteHeader(http.StatusOK)
+}
+
+func devlHandler(w http.ResponseWriter, r *http.Request) {
+	var ansi ansible.Runner
+	params := ansible.Params{
+		User:	"jynx",
+		Log:	"goansi.log",
+		Cmd:	ansible.Cmd{
+			AnsibleCommand: "playbook",
+			Args: []string{	"-u", "jynx", "--tags", "update-images", "workstation.yml" },
 		},
 	}
 
